f14fab293a00ccfa0a9f6a5874e7cda161230979
Dejoe John <22582871+dej0e@users.noreply.github.com>
2025-07-31 12:15:05 -0400

    Time: 264 ms (36.35%), Space: 20 MB (82.48%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -3,38 +3,39 @@ from collections import deque
 
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-        num_cols, num_rows = len(grid[0]), len(grid)
-        visited = [[False for c in range(num_cols)] for r in range(num_rows)]
+        num_rows = len(grid)
+        num_cols = len(grid[0])
 
-        def get_neighbors(coord):
-            res = []
-            row, col = coord
+        def get_neighbor(node):
+            r, c = node
+            neighbors = []
             delta_row = [-1, 0, 1, 0]
             delta_col = [0, 1, 0, -1]
             for i in range(len(delta_row)):
-                neighbor_c = col + delta_col[i]
-                neighbor_r = row + delta_row[i]
-                if 0 <= neighbor_r < num_rows and 0 <= neighbor_c < num_cols:
-                    res.append((neighbor_r, neighbor_c))
-            return res
+                nei_r = r + delta_row[i]
+                nei_c = c + delta_col[i]
+                if 0 <= nei_r < num_rows and 0 <= nei_c < num_cols:
+                    neighbors.append((nei_r, nei_c))
+            return neighbors
 
-        def bfs(start):
-            queue = deque([start])
-            r, c = start
-            visited[r][c] = True
+        def bfs(root):
+            queue = deque([root])
+            r, c = root
+
+            grid[r][c] = "0"
             while len(queue) > 0:
                 node = queue.popleft()
-                for neighbor in get_neighbors(node):
+                for neighbor in get_neighbor(node):
                     r, c = neighbor
-                    if grid[r][c] == '0' or visited[r][c]:
+                    if grid[r][c] == "0":
                         continue
+                    grid[r][c] = "0"
                     queue.append(neighbor)
-                    visited[r][c] = True
 
         count = 0
         for r in range(num_rows):
             for c in range(num_cols):
-                if grid[r][c] == '0' or visited[r][c]:
+                if grid[r][c] == "0":
                     continue
                 bfs((r, c))
                 count += 1
