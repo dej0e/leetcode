3f0755f587222f2b7c6e0d20f121c8c4cf57b593
Dejoe John <22582871+dej0e@users.noreply.github.com>
2025-08-12 12:19:39 -0400

    Time: 131 ms (61.3%), Space: 32.9 MB (76.2%) - LeetHub

--- 0221-maximal-square/0221-maximal-square.py
+++ 0221-maximal-square/0221-maximal-square.py
@@ -27,3 +27,25 @@ class Solution:
                 best = max(dp[r][c], best)
 
         return best * best
+
+# For convenience, we'll say a square ends in a cell if its bottom-right corner is located in that cell.
+
+# If the cell at (r, c) is 0 then there's no 1-filled square that ends in this cell. Otherwise, we have to figure out how to compute dp[r][c] from previous subproblems.
+
+# Let's consider a cell (r,c) where matrix[r][c] = 1 and dp[r][c] = k for some positive integer k.
+
+# For a square of size k to exist at the cell (r,c), then these conditions must be also true:
+
+#     A square of size k - 1 that ends at (r - 1, c).
+#     A square of size k - 1 that ends at (r, c - 1).
+#     A square of size k - 1 that ends at (r - 1, c - 1).
+
+# For a square to have size k - 1 at the cells (r - 1, c), (r, c - 1), and (r - 1, c - 1), the values dp[r - 1][c], dp[r][c - 1], and dp[r - 1][c - 1] all have to be greater or equal than k - 1.
+
+# From this observation, we can obtain the following transition:
+
+# dp[r][c] = 1 + min(dp[r - 1][c], dp[r][c - 1], dp[r - 1][c - 1])
+
+# This state essentially means look at the minimum sized square that ends at one of the following cells: (r - 1, c), (r, c - 1), (r - 1, c - 1) Take that value and after adding 1, we can obtain dp[r][c].
+
+# Time Complexity: O(r*c)
