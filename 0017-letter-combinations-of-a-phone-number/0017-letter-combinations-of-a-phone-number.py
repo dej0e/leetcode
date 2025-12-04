013d79ec8cf63eb73013d4a0f8dd6ff16762f6f4
Dejoe John <22582871+dej0e@users.noreply.github.com>
2025-12-04 14:00:59 -0800

    Time: 0 ms (100%), Space: 17.8 MB (67.19%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.py
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.py
@@ -1,26 +1,23 @@
 class Solution:
     def letterCombinations(self, digits: str) -> List[str]:
-        KEYBOARD = {
-            "2": "abc",
-            "3": "def",
-            "4": "ghi",
-            "5": "jkl",
-            "6": "mno",
-            "7": "pqrs",
-            "8": "tuv",
-            "9": "wxyz",
+        res = []
+        digit_map = {
+            "2": ["a", "b", "c"],
+            "3": ["d", "e", "f"],
+            "4": ["g", "h", "i"],
+            "5": ["j", "k", "l"],
+            "6": ["m", "n", "o"],
+            "7": ["p", "q", "r", "s"],
+            "8": ["t", "u", "v"],
+            "9": ["w", "x", "y", "z"],
         }
 
-        res: List[str] = []
-        if not digits:
-            return []
         def dfs(start_index, path):
             if start_index == len(digits):
                 res.append("".join(path))
                 return
-
             next_number = digits[start_index]
-            for letter in KEYBOARD[next_number]:
+            for letter in digit_map[next_number]:
                 path.append(letter)
                 dfs(start_index + 1, path)
                 path.pop()
