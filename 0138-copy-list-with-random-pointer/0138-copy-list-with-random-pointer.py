195ec832262cfe1d87e7e25064ccea9195d778f4
Dejoe John <22582871+dej0e@users.noreply.github.com>
2026-01-15 21:46:07 -0800

    Time: 42 ms (52.73%), Space: 20.2 MB (10.6%) - LeetHub

---
+++ 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.py
@@ -0,0 +1,42 @@
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, x: int, next: 'Node' = None, random: 'Node' = None):
+        self.val = int(x)
+        self.next = next
+        self.random = random
+"""
+
+
+class Solution:
+    def copyRandomList(self, head: "Optional[Node]") -> "Optional[Node]":
+        old_newnode_map = {}
+        curr_old = head
+        curr_new = None
+        new_head = None
+        while curr_old:
+            new_node = Node(x=curr_old.val)
+            curr_old_next = curr_old.next
+            curr_old_random = curr_old.random
+            if curr_old_random in old_newnode_map:
+                new_node.random = old_newnode_map.get(curr_old_random)
+
+            if curr_new:
+                curr_new.next = new_node
+            else:
+                new_head = new_node
+            curr_new = new_node
+            old_newnode_map[curr_old] = new_node
+            curr_old = curr_old.next
+
+        curr_new = new_head
+        new_oldnode_map = {v: k for k, v in old_newnode_map.items()}
+
+        curr_old = head
+        curr_new = new_head
+        while curr_old:
+            curr_new.random = old_newnode_map.get(curr_old.random) 
+            curr_old = curr_old.next
+            curr_new = curr_new.next
+
+        return new_head
