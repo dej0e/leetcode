a1a877c8e2b073b8fa0c2b945f5b3e697d713d19
Dejoe John <22582871+dej0e@users.noreply.github.com>
2026-01-22 13:07:32 -0800

    Time: 7 ms (30.21%), Space: 19.4 MB (12.67%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.py
+++ 0002-add-two-numbers/0002-add-two-numbers.py
@@ -4,24 +4,24 @@
 #         self.val = val
 #         self.next = next
 class Solution:
-    def addTwoNumbersAndCarry(
-        self, l1: Optional[ListNode], l2: Optional[ListNode], carry: int
-    ) -> Optional[ListNode]:
-        if not l1 and not l2 and carry == 0:
-            return None
-        v1 = l1.val if l1 else 0
-        v2 = l2.val if l2 else 0
-        total = v1 + v2 + carry
-        carry = total // 10
-        val = total % 10
-        next_node = self.addTwoNumbersAndCarry(
-            l1.next if l1 else None, 
-            l2.next if l2 else None, 
-            carry
-        )
-        return ListNode(val, next_node)
-
     def addTwoNumbers(
         self, l1: Optional[ListNode], l2: Optional[ListNode]
     ) -> Optional[ListNode]:
-        return self.addTwoNumbersAndCarry(l1, l2, 0)
+        dummy = ListNode()
+        cur = dummy
+        carry = 0
+        while l1 or l2 or carry:
+            val1 = l1.val if l1 else 0
+            val2 = l2.val if l2 else 0
+
+            val = val1 + val2 + carry
+            carry = val // 10
+            val = val % 10
+            cur.next = ListNode(val)
+
+            cur = cur.next
+            l1 = l1.next if l1 else None
+            l2 = l2.next if l2 else None
+
+        return dummy.next
+            
