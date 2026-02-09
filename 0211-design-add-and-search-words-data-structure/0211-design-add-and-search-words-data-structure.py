0fc29119cf30d2ef04903d0e28ea435b713424a0
Dejoe John <22582871+dej0e@users.noreply.github.com>
2026-02-09 11:33:42 -0800

    Time: 1522 ms (15.47%), Space: 67.2 MB (53.79%) - LeetHub

---
+++ 0211-design-add-and-search-words-data-structure/0211-design-add-and-search-words-data-structure.py
@@ -0,0 +1,61 @@
+class TrieNode:
+    def __init__(self):
+        self.children = {}
+        self.endOfWord = False
+
+
+class WordDictionary:
+
+    def __init__(self):
+        self.root = TrieNode()
+
+    def addWord(self, word: str) -> None:
+        cur = self.root
+        for c in word:
+            if c not in cur.children:
+                cur.children[c] = TrieNode()
+            cur = cur.children[c]
+        cur.endOfWord = True
+
+    def search(self, word: str) -> bool:
+        cur = self.root
+        for idx, c in enumerate(word):
+            if c == ".":
+                dotAlphaMap = {}
+                for key in cur.children.keys():
+                    dotAlphaMap[key] = self.searchFromNode(
+                        cur.children[key], word, idx + 1
+                    )
+                for key, value in dotAlphaMap.items():
+                    if value == True:
+                        return True
+                return False
+            elif c not in cur.children:
+                return False
+            cur = cur.children[c]
+        return cur.endOfWord
+
+    def searchFromNode(self, node: TrieNode, word: str, startIndex) -> bool:
+        cur = node
+        for i in range(startIndex, len(word)):
+            c = word[i]
+            if c == ".":
+                dotAlphaMap = {}
+                for key in cur.children.keys():
+                    dotAlphaMap[key] = self.searchFromNode(
+                        cur.children[key], word, i + 1
+                    )
+                for key, value in dotAlphaMap.items():
+                    if value == True:
+                        return True
+                return False
+            elif c not in cur.children:
+                return False
+            cur = cur.children[c]
+        return cur.endOfWord
+
+
+# Your WordDictionary object will be instantiated and called as such:
+# obj = WordDictionary()
+# obj.addWord(word)
+# param_2 = obj.search(word)
