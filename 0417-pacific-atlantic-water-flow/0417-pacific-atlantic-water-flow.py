12d267731980e04dc329e241fd9ab35d9b297b6a
Dejoe John <22582871+dej0e@users.noreply.github.com>
2026-02-11 10:58:07 -0800

    Time: 50 ms (19.57%), Space: 21 MB (41.36%) - LeetHub

--- 0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.py
+++ 0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.py
@@ -3,51 +3,43 @@ from collections import deque
 
 class Solution:
     def pacificAtlantic(self, heights: List[List[int]]) -> List[List[int]]:
-
-        num_rows = len(heights)
-        num_cols = len(heights[0])
-        pac = [[False] * num_cols for _ in range(num_rows)]
-        atl = [[False] * num_cols for _ in range(num_rows)]
-
-        def get_neighbors(r, c, ocean):
-            delta_row = [-1, 0, 1, 0]
-            delta_col = [0, 1, 0, -1]
-            for i in range(len(delta_row)):
-                nr = r + delta_row[i]
-                nc = c + delta_col[i]
-                if (
-                    0 <= nr < num_rows
-                    and 0 <= nc < num_cols
-                    and heights[r][c] <= heights[nr][nc]
-                    and ocean[nr][nc] == False
-                ):
-                    yield nr, nc
+        ROWS, COLS = len(heights), len(heights[0])
+        pac = [[False] * COLS for _ in range(ROWS)]
+        atl = [[False] * COLS for _ in range(ROWS)]
+        directions = [(1, 0), (-1, 0), (0, 1), (0, -1)]
 
         def bfs(source, ocean):
-            queue = deque(source)
-            while queue:
-                r, c = queue.popleft()
+            q = deque()
+            q.extend(source)
+            while q:
+                r, c = q.popleft()
                 ocean[r][c] = True
-                for nr, nc in get_neighbors(r, c, ocean):
-                    queue.append((nr, nc))
+                for dr, dc in directions:
+                    nr = r + dr
+                    nc = c + dc
+                    if (
+                        0 <= nr < ROWS
+                        and 0 <= nc < COLS
+                        and ocean[nr][nc] == False
+                        and heights[nr][nc] >= heights[r][c]
+                    ):
+                        q.append((nr, nc))
 
         pacific = []
         atlantic = []
-
-        for c in range(num_cols):
-            pacific.append((0, c))
-            atlantic.append((num_rows - 1, c))
-
-        for r in range(num_rows):
+        for r in range(ROWS):
             pacific.append((r, 0))
-            atlantic.append((r, num_cols - 1))
+            atlantic.append((r, COLS - 1))
 
+        for c in range(COLS):
+            pacific.append((0, c))
+            atlantic.append((ROWS - 1, c))
+        
         bfs(pacific, pac)
         bfs(atlantic, atl)
-
         res = []
-        for r in range(num_rows):
-            for c in range(num_cols):
-                if atl[r][c] and pac[r][c]:
-                    res.append((r, c))
-        return res
+        for r in range(ROWS):
+            for c in range(COLS):
+               if pac[r][c] and atl[r][c]:
+                res.append([r,c])
+        return res 
