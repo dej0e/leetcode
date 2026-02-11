55ec176b1723d35533027bc4844816503860f89d
Dejoe John <22582871+dej0e@users.noreply.github.com>
2026-02-10 17:13:01 -0800

    Time: 228 ms (92.03%), Space: 21.7 MB (43.99%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -3,40 +3,30 @@ from collections import deque
 
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-        num_rows = len(grid)
-        num_cols = len(grid[0])
-
-        def get_neighbor(node):
-            r, c = node
-            neighbors = []
-            delta_row = [-1, 0, 1, 0]
-            delta_col = [0, 1, 0, -1]
-            for i in range(len(delta_row)):
-                nei_r = r + delta_row[i]
-                nei_c = c + delta_col[i]
-                if 0 <= nei_r < num_rows and 0 <= nei_c < num_cols:
-                    neighbors.append((nei_r, nei_c))
-            return neighbors
-
-        def bfs(root):
-            queue = deque([root])
-            r, c = root
-
-            grid[r][c] = "0"
-            while len(queue) > 0:
-                node = queue.popleft()
-                for neighbor in get_neighbor(node):
-                    r, c = neighbor
-                    if grid[r][c] == "0":
-                        continue
-                    grid[r][c] = "0"
-                    queue.append(neighbor)
+        if not grid:
+            return 0
+        ROWS, COLS = len(grid), len(grid[0])
 
         count = 0
-        for r in range(num_rows):
-            for c in range(num_cols):
-                if grid[r][c] == "0":
-                    continue
-                bfs((r, c))
-                count += 1
+
+        def bfs(r, c):
+            if r < 0 or c < 0 or r >= ROWS or c >= COLS or grid[r][c] == "0":
+                return
+            q = deque([(r, c)])
+            grid[r][c] = 0
+            delta = [(-1, 0), (0, 1), (1, 0), (0, -1)]
+            while q:
+                r, c = q.popleft()
+                for dr, dc in delta:
+                    nr = r+dr
+                    nc = c+dc
+                    if 0 <= nr < ROWS and 0 <= nc < COLS and grid[nr][nc] =="1":
+                        q.append((nr,nc))
+                        grid[nr][nc] = '0'
+
+        for r in range(ROWS):
+            for c in range(COLS):
+                if grid[r][c] == "1":
+                    bfs(r, c)
+                    count += 1
         return count
