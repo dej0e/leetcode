393f464765ac2818f7ffc7eafe62473321143025
Dejoe John <22582871+dej0e@users.noreply.github.com>
2026-02-16 11:13:07 -0800

    Time: 481 ms (80.75%), Space: 19.7 MB (46.32%) - LeetHub

--- 0322-coin-change/0322-coin-change.py
+++ 0322-coin-change/0322-coin-change.py
@@ -3,8 +3,9 @@ import math
 
 class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
-
+            
         dp = [math.inf] * (amount + 1)
+            # defaultval
         dp[0] = 0
         for a in range(1, amount + 1):
             for c in coins:
