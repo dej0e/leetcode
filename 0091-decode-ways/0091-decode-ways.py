286a3fbfaf90e39a2a48cb60d37984cf445c9230
Dejoe John <22582871+dej0e@users.noreply.github.com>
2026-02-17 10:24:30 -0800

    Time: 3 ms (27.63%), Space: 19.6 MB (10.13%) - LeetHub

--- 0091-decode-ways/0091-decode-ways.py
+++ 0091-decode-ways/0091-decode-ways.py
@@ -18,15 +18,21 @@ class Solution:
         ## TOP DOWN
         n = len(s)
         dp = {n: 1}
+
         def dfs(i):
             if i in dp:
                 return dp[i]
-            if s[i] == '0':
+
+            if s[i] == "0":
                 return 0
+
             res = 0
-            res = dfs(i+1)
-            if i+1 < n and ((s[i]=="1") or (s[i]=='2' and s[i+1] in "0123456")):
-                res += dfs(i+2)
+            res = dfs(i + 1)
+
+            if i + 1 < n and ((s[i] == "1") or (s[i] == "2" and s[i + 1] in "0123456")):
+                res += dfs(i + 2)
+
             dp[i] = res
             return dp[i]
+
         return dfs(0)
