f871326dab870a379e1be3b9b988be0686ff1a70
Dejoe John <22582871+dej0e@users.noreply.github.com>
2026-03-02 11:15:12 -0800

    Time: 30 ms (74.1%), Space: 21 MB (85.62%) - LeetHub

--- 0846-hand-of-straights/0846-hand-of-straights.py
+++ 0846-hand-of-straights/0846-hand-of-straights.py
@@ -9,9 +9,9 @@ class Solution:
             while count[start-1] > 0:
                 start=start-1
             while start <= num:
-                while count[start] > 0:
+                while count[start]:
                     for i in range(start, start + groupSize):
-                        if count[i] <= 0:
+                        if not count[i]:
                             return False
                         count[i] -= 1
                 start += 1
