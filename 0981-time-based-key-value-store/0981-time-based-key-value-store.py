6dbb5d879361079c482b99029524bc55014144b1
Dejoe John <22582871+dej0e@users.noreply.github.com>
2026-03-16 11:57:37 -0700

    Time: 143 ms (39.44%), Space: 69.9 MB (18.56%) - LeetHub

---
+++ 0981-time-based-key-value-store/0981-time-based-key-value-store.py
@@ -0,0 +1,31 @@
+class TimeMap:
+
+    def __init__(self):
+        self.keyStore = {}
+
+    def set(self, key: str, value: str, timestamp: int) -> None:
+        if key not in self.keyStore:
+            self.keyStore[key] = []
+        self.keyStore[key].append([value, timestamp])
+        # self.keyStore[key][0] -- value
+        # self.keyStore[key][1] -- timestamp
+        
+    def get(self, key: str, timestamp: int) -> str:
+        res = ""
+        values = self.keyStore.get(key, [])
+        l, r = 0, len(values) - 1
+        while l <= r:
+            mid = (l + r) // 2
+            if values[mid][1] <= timestamp:
+                res = values[mid][0]
+                l = mid + 1
+            else:
+                r = mid - 1
+        return res
+
+
+
+# Your TimeMap object will be instantiated and called as such:
+# obj = TimeMap()
+# obj.set(key,value,timestamp)
+# param_2 = obj.get(key,timestamp)
