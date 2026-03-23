5eb72f304bcfee9711b51dd86cbd64a111b41983
Dejoe John <22582871+dej0e@users.noreply.github.com>
2026-03-23 10:55:31 -0700

    Time: 43 ms (62.31%), Space: 34.4 MB (18.28%) - LeetHub

--- 0131-palindrome-partitioning/0131-palindrome-partitioning.py
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.py
@@ -1,22 +1,25 @@
 class Solution:
     def partition(self, s: str) -> List[List[str]]:
         res = []
-        n = len(s)
-        def is_palindrome(word:str):
-            return word == word[::-1]
-
-        def dfs(i, part):
-            if i >= n:
+        part = []
+        def is_palindrome(s, i, j):
+            
+            while i < j:
+                if s[i] != s[j]:
+                    return False
+                i += 1
+                j -= 1
+            return True
+        
+        def dfs(i):
+            if i == len(s):
                 res.append(part.copy())
                 return
-
-            for j in range(i, n):
-                prefix = s[i:j+1]
-                if not is_palindrome(prefix):
-                    continue
-                part.append(prefix)
-                dfs(j+1, part)
-                part.pop()
-
-        dfs(0,[])
+            
+            for j in range(i, len(s)):
+                if is_palindrome(s, i, j):
+                    part.append(s[i: j + 1])
+                    dfs(j + 1)
+                    part.pop()
+        dfs(0)
         return res
