4cc9c063417f328fd19771b2db1f3d6766c5f12e
Dejoe John <22582871+dej0e@users.noreply.github.com>
2026-03-23 10:26:08 -0700

    Time: 4218 ms (39.42%), Space: 19.4 MB (75.21%) - LeetHub

--- 0079-word-search/0079-word-search.py
+++ 0079-word-search/0079-word-search.py
@@ -1,46 +1,34 @@
 class Solution:
     def exist(self, board: List[List[str]], word: str) -> bool:
-        num_rows, num_cols = len(board), len(board[0])
-        visited = [[False for c in range(num_cols)] for r in range(num_rows)]
-        def get_neighbor(row, col):
-            neighbors = []
-            # i-1, j
-            # i, j+1
-            # i+1, j
-            # i, j-1
-            delta_row = [-1, 0, 1, 0]
-            delta_col = [0, 1, 0, -1]
-            for i in range(len(delta_row)):
-                neighbor_row = row + delta_row[i]
-                neighbor_col = col + delta_col[i]
-                if 0 <= neighbor_row < num_rows and 0 <= neighbor_col < num_cols:
-                    if not visited[neighbor_row][neighbor_col]:
-                        neighbors.append((neighbor_row, neighbor_col))
-            return neighbors
         
-        def dfs(i, path, row, col):
-            current_path_word = "".join(path)
-            if i == len(word)-1 and current_path_word == word:
-                return True
-            
-            found = False
-            visited[row][col] = True
-            for nei_row, nei_col in get_neighbor(row, col):
-                if board[nei_row][nei_col] == word[i+1]:
-                    path.append(board[nei_row][nei_col])
-                    found = found or dfs(i+1, path, nei_row, nei_col)
-                    path.pop()
+        ROWS = len(board)
+        COLS = len(board[0])
+        visited = [[False] * COLS for _ in range(ROWS)]
 
-                    if found:
-                        return True
-            visited[row][col] = False
-            return found
+        def dfs(r, c, i):
+            if i == len(word):
+                return True
+
+            if (r < 0 or c < 0 or r >= ROWS or c >= COLS):
+                return False
+            
+            if visited[r][c] or board[r][c] != word[i]:
+                return False
+            
+            
+            visited[r][c] = True
+            directions = [(-1, 0), (0, 1), (1, 0), (0, -1)]
+            
+            res = False
+            for dr, dc in directions:
+                nr, nc = r + dr, c + dc
+                if dfs(nr, nc, i + 1):
+                    return True
+            visited[r][c] = False
+            return False
         
-        for r in range(num_rows):
-            for c in range(num_cols):
-                if board[r][c] != word[0]:
-                    continue
-                if dfs(0, [board[r][c]], r, c):
+        for r in range(ROWS):
+            for c in range(COLS):
+                if dfs(r, c, 0):
                     return True
         return False
-
