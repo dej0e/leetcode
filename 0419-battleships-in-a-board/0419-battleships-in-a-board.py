2265529e4dd2ac8dcfc11fe021920f603fedc275
Dejoe John <22582871+dej0e@users.noreply.github.com>
2026-03-23 21:08:48 -0700

    Time: 3 ms (55.28%), Space: 21.2 MB (66.92%) - LeetHub

---
+++ 0419-battleships-in-a-board/0419-battleships-in-a-board.py
@@ -0,0 +1,23 @@
+class Solution:
+    def countBattleships(self, board: List[List[str]]) -> int:
+        ROWS = len(board)
+        COLS = len(board[0])
+        directions = [(0, -1), (0, 1), (-1, 0), (1, 0)]
+
+        def dfs(r, c):
+            if min(r, c) < 0 or r >= ROWS or c >= COLS or board[r][c] != "X":
+                return
+
+            board[r][c] = "."
+            for dr, dc in directions:
+                nr = r + dr
+                nc = c + dc
+                dfs(nr, nc)
+
+        count = 0
+        for r in range(ROWS):
+            for c in range(COLS):
+                if board[r][c] == "X":
+                    dfs(r, c)
+                    count += 1
+        return count
