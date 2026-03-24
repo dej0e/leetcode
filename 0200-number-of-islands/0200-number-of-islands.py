ae664edbd5954ed2f903bdd4b82222453fe144c5
Dejoe John <22582871+dej0e@users.noreply.github.com>
2026-03-23 19:15:21 -0700

    Time: 246 ms (53.74%), Space: 21.5 MB (84.46%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -6,30 +6,29 @@ class Solution:
         if not grid:
             return 0
         ROWS, COLS = len(grid), len(grid[0])
-
         count = 0
+
         directions = [(-1, 0), (0, 1), (1, 0), (0, -1)]
         def bfs(row, col):
             q = deque()
-            grid[row][col] = 0
             q.append((row, col))
             while q:
                 r, c = q.popleft()
-                
                 for dr, dc in directions:
                     nr = r + dr
-                    nc = c + dc
-                    if min(nr, nc) < 0 or nr >= ROWS or nc >= COLS or grid[nr][nc] != "1":
+                    nc = c + dc 
+                    
+                    # check for valid cases
+                    if min(nr, nc) < 0 or nr >= ROWS or nc >= COLS or grid[nr][nc] == "0":
                         continue
+                    
                     q.append((nr, nc))
                     grid[nr][nc] = "0"
-        
+            
+             
         for r in range(ROWS):
             for c in range(COLS):
                 if grid[r][c] == "1":
                     bfs(r, c)
                     count += 1
         return count
-                
-                
-            
