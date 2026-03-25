ae8790e3071a7b97adfaf947aa34d8e9e694045d
Dejoe John <22582871+dej0e@users.noreply.github.com>
2026-03-25 11:37:00 -0700

    Time: 3 ms (89.14%), Space: 20.4 MB (86.92%) - LeetHub

--- 0323-number-of-connected-components-in-an-undirected-graph/0323-number-of-connected-components-in-an-undirected-graph.py
+++ 0323-number-of-connected-components-in-an-undirected-graph/0323-number-of-connected-components-in-an-undirected-graph.py
@@ -1,27 +1,30 @@
 class Solution:
     def countComponents(self, n: int, edges: List[List[int]]) -> int:
-        adj = defaultdict(list)
-        for src, dst in edges:
-            adj[src].append(dst)
-            adj[dst].append(src)
+        adj = {i: [] for i in range(n)}
+        for a, b in edges:
+            adj[a].append(b)
+            adj[b].append(a)
+        
 
-        visited = [False] * n
-        components = 0
 
-        def bfs(node):
-            nonlocal components
-            q = deque([node])
-            visited[node] = True
+        visited = set()
+        def bfs(i):
+            q = deque()
+            q.append(i)
+            visited.add(i)
             while q:
-                cur = q.popleft()
-                for nei in adj[cur]:
-                    if visited[nei]:
+                node = q.popleft()
+                for neighbor in adj[node]:
+                    if neighbor in visited:
                         continue
-                    visited[nei] = True
-                    q.append(nei)
-            components += 1
-
+                    q.append(neighbor)
+                    visited.add(neighbor)
+        
+        components = 0
         for i in range(n):
-            if visited[i] == False:
+            if i not in visited:
                 bfs(i)
+                components += 1
         return components
+
+        
