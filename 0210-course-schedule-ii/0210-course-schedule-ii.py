7f8cd0955874fc69d71f45d4f8019cd8595f4936
Dejoe John <22582871+dej0e@users.noreply.github.com>
2026-03-24 21:20:33 -0700

    Time: 3 ms (76.17%), Space: 21.3 MB (17.7%) - LeetHub

--- 0210-course-schedule-ii/0210-course-schedule-ii.py
+++ 0210-course-schedule-ii/0210-course-schedule-ii.py
@@ -3,39 +3,34 @@ from enum import Enum
 
 class Solution:
     def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
-        class State(Enum):
-            NOT_VISITED = 0
-            VISITED = 1
-            VISITING = 2
-
-        graph = {}
-        states = {}
-        for node in range(numCourses):
-            graph[node] = []
-            states[node] = State.NOT_VISITED
-
-        for subject, prereq in prerequisites:
-            graph[subject].append(prereq)
-
-        path = []
-
-        def dfs(start):
-            #Cycle in Graph
-            if states[start] == State.VISITING:
+        prereq = {i:[] for i in range(numCourses)}
+        for course, pr in prerequisites:
+            prereq[course].append(pr)
+        
+        res = []
+        completed = set()
+        cycle = set()
+        def dfs(course):
+            if course in cycle:
                 return False
-            #Already completed prerequisite
-            if states[start] == State.VISITED:
+            
+            if course in completed:
                 return True
-
-            states[start] = State.VISITING
-            for prereq in graph[start]:
-                if not dfs(prereq):
+            
+            
+            cycle.add(course)
+            for p in prereq[course]:
+                if not dfs(p):
                     return False
-            states[start] = State.VISITED
-            path.append(start)
+            cycle.remove(course)
+            completed.add(course)
+            res.append(course)
             return True
 
         for i in range(numCourses):
-            if states[i] == State.NOT_VISITED and not dfs(i):
+            if not dfs(i):
                 return []
-        return path
+        return res
+
+
+        
