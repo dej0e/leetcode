f6580c72c57a618549da61112833a9d10082493b
Dejoe John <22582871+dej0e@users.noreply.github.com>
2026-03-25 13:16:39 -0700

    Time: 7 ms (49.3%), Space: 21.2 MB (30.62%) - LeetHub

--- 0323-number-of-connected-components-in-an-undirected-graph/0323-number-of-connected-components-in-an-undirected-graph.py
+++ 0323-number-of-connected-components-in-an-undirected-graph/0323-number-of-connected-components-in-an-undirected-graph.py
@@ -5,26 +5,40 @@ class Solution:
             adj[a].append(b)
             adj[b].append(a)
         
+        visited = [False] * n
 
-
-        visited = set()
-        def bfs(i):
-            q = deque()
-            q.append(i)
-            visited.add(i)
-            while q:
-                node = q.popleft()
-                for neighbor in adj[node]:
-                    if neighbor in visited:
-                        continue
-                    q.append(neighbor)
-                    visited.add(neighbor)
+        def dfs(i):
+            if visited[i] == True:
+                return
+            visited[i] = True
+            for neighbor in adj[i]:
+                dfs(neighbor)
         
         components = 0
         for i in range(n):
-            if i not in visited:
-                bfs(i)
+            if visited[i] != True:
+                dfs(i)
                 components += 1
         return components
+
+        # visited = set()
+        # def bfs(i):
+        #     q = deque()
+        #     q.append(i)
+        #     visited.add(i)
+        #     while q:
+        #         node = q.popleft()
+        #         for neighbor in adj[node]:
+        #             if neighbor in visited:
+        #                 continue
+        #             q.append(neighbor)
+        #             visited.add(neighbor)
+        
+        # components = 0
+        # for i in range(n):
+        #     if i not in visited:
+        #         bfs(i)
+        #         components += 1
+        # return components
 
         
