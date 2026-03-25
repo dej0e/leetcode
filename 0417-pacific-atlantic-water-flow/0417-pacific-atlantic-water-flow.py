60ec1b2bc825551159e40e5a24006ff29e73b3ec
Dejoe John <22582871+dej0e@users.noreply.github.com>
2026-03-24 19:33:46 -0700

    Time: 47 ms (19.55%), Space: 20.8 MB (72.41%) - LeetHub

--- 0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.py
+++ 0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.py
@@ -4,41 +4,40 @@ from collections import deque
 class Solution:
     def pacificAtlantic(self, heights: List[List[int]]) -> List[List[int]]:
         ROWS, COLS = len(heights), len(heights[0])
-        pac = [[False] * COLS for _ in range(ROWS)]
-        atl = [[False] * COLS for _ in range(ROWS)]
-        directions = [(1, 0), (-1, 0), (0, 1), (0, -1)]
+        pacVisit = [[False] * COLS for _ in range(ROWS)]
+        atlVisit = [[False] * COLS for _ in range(ROWS)]
 
+        pac = []
+        atl = []
+        for r in range(ROWS):
+            atl.append((r, COLS-1))
+            pac.append((r, 0))
+
+        for c in range(COLS):
+            atl.append((ROWS-1, c))
+            pac.append((0, c))
+
+        directions = [(-1, 0), (0, 1), (1, 0), (0, -1)]
         def bfs(source, ocean):
             q = deque(source)
             while q:
                 r, c = q.popleft()
                 ocean[r][c] = True
                 for dr, dc in directions:
-                    nr = r + dr
-                    nc = c + dc
-                    if (
-                        0 <= nr < ROWS
-                        and 0 <= nc < COLS
-                        and ocean[nr][nc] == False
-                        and heights[nr][nc] >= heights[r][c]
-                    ):
-                        q.append((nr, nc))
+                    nr = dr + r
+                    nc = dc + c
+                    if min(nr, nc) < 0 or nr >= ROWS or nc >= COLS:
+                        continue
+                    if ocean[nr][nc] == True or heights[nr][nc] < heights[r][c]:
+                        continue
+                    q.append((nr, nc))
 
-        pacific = []
-        atlantic = []
-        for r in range(ROWS):
-            pacific.append((r, 0))
-            atlantic.append((r, COLS - 1))
-
-        for c in range(COLS):
-            pacific.append((0, c))
-            atlantic.append((ROWS - 1, c))
-        
-        bfs(pacific, pac)
-        bfs(atlantic, atl)
+        bfs(atl, atlVisit)
+        bfs(pac, pacVisit)
         res = []
         for r in range(ROWS):
             for c in range(COLS):
-               if pac[r][c] and atl[r][c]:
-                res.append([r,c])
-        return res 
+                if pacVisit[r][c] and atlVisit[r][c]:
+                    res.append((r, c))
+        return res
+        
