3c7caf696366bf8ae5584f464a1dabd9fe359d4f
Dejoe John <22582871+dej0e@users.noreply.github.com>
2026-03-28 09:37:19 -0700

    Time: 39 ms (28.48%), Space: 20.7 MB (82.89%) - LeetHub

--- 0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.py
+++ 0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.py
@@ -3,41 +3,40 @@ from collections import deque
 
 class Solution:
     def pacificAtlantic(self, heights: List[List[int]]) -> List[List[int]]:
-        ROWS, COLS = len(heights), len(heights[0])
+        ROWS = len(heights)
+        COLS = len(heights[0])
         pacVisit = [[False] * COLS for _ in range(ROWS)]
         atlVisit = [[False] * COLS for _ in range(ROWS)]
-
         pac = []
         atl = []
         for r in range(ROWS):
-            atl.append((r, COLS-1))
             pac.append((r, 0))
+            atl.append((r, COLS - 1))
 
         for c in range(COLS):
-            atl.append((ROWS-1, c))
             pac.append((0, c))
+            atl.append((ROWS - 1, c))
 
         directions = [(-1, 0), (0, 1), (1, 0), (0, -1)]
+
         def bfs(source, ocean):
             q = deque(source)
             while q:
                 r, c = q.popleft()
                 ocean[r][c] = True
+
                 for dr, dc in directions:
                     nr = dr + r
                     nc = dc + c
-                    if min(nr, nc) < 0 or nr >= ROWS or nc >= COLS:
-                        continue
-                    if ocean[nr][nc] == True or heights[nr][nc] < heights[r][c]:
-                        continue
-                    q.append((nr, nc))
-                    ocean[nr][nc] = True
+                    if 0 <= nr < ROWS and 0 <= nc < COLS and ocean[nr][nc] != True and heights[nr][nc] >= heights[r][c]:
+                        q.append((nr, nc))
 
-        bfs(atl, atlVisit)
-        bfs(pac, pacVisit)
         res = []
+        bfs(pac, pacVisit)
+        bfs(atl, atlVisit)
+
         for r in range(ROWS):
             for c in range(COLS):
-                if pacVisit[r][c] and atlVisit[r][c]:
+                if pacVisit[r][c] == True and atlVisit[r][c] == True:
                     res.append((r, c))
         return res
