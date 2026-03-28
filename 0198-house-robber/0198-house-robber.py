69cf3661293e6314652560b79e337811920101d4
Dejoe John <22582871+dej0e@users.noreply.github.com>
2026-03-28 16:26:35 -0700

    Time: 0 ms (100%), Space: 19.2 MB (76.12%) - LeetHub

--- 0198-house-robber/0198-house-robber.py
+++ 0198-house-robber/0198-house-robber.py
@@ -1,5 +1,6 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
+        # Bottom-up approach
         # dp = [0] * len(nums)
         # if len(nums) <= 2:
         #     return max(nums)
@@ -11,6 +12,8 @@ class Solution:
         # return dp[-1]
 
         rob1, rob2 = 0, 0
+        # (rob1, rob2, n, n+1, ..) is simialr to
+        # dp[n-2], dp[n-1], dp[n], ..)
         for n in nums:
             temp = max(rob1 + n, rob2)
             rob1 = rob2
