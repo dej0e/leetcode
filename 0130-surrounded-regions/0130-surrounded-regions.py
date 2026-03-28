99bc5733917ada8a72b0e92522f310b18e92e521
Dejoe John <22582871+dej0e@users.noreply.github.com>
2026-03-28 12:27:48 -0700

    Time: 7 ms (54.79%), Space: 22.4 MB (68.72%) - LeetHub

--- 0130-surrounded-regions/0130-surrounded-regions.py
+++ 0130-surrounded-regions/0130-surrounded-regions.py
@@ -11,30 +11,29 @@ class Solution:
         directions = [(-1, 0), (0, 1), (1, 0), (0, -1)]
         visited = set()
 
-        def bfs():
-            q = deque()
-            for r in range(ROWS):
-                for c in range(COLS):
-                    if (r == 0 or r == ROWS - 1 or c == 0 or c == COLS - 1) and board[r][c] == "O":
-                        q.append((r, c))
-            while q:
-                r, c = q.popleft()
-                visited.add((r, c))
-                board[r][c] = "T"
-                for dr, dc in directions:
-                    nr = dr + r
-                    nc = dc + c
-                    if (
-                        min(nr, nc) < 0
-                        or nr >= ROWS
-                        or nc >= COLS
-                        or (nr, nc) in visited
-                        or board[nr][nc] != "O"
-                    ):
-                        continue
-                    q.append((nr, nc))
+        q = deque()
+        for r in range(ROWS):
+            for c in range(COLS):
+                if (r == 0 or r == ROWS - 1 or c == 0 or c == COLS - 1) and board[r][c] == "O":
+                    q.append((r, c))
+        while q:
+            r, c = q.popleft()
+            visited.add((r, c))
+            board[r][c] = "T"
+            for dr, dc in directions:
+                nr = dr + r
+                nc = dc + c
+                if (
+                    min(nr, nc) < 0
+                    or nr >= ROWS
+                    or nc >= COLS
+                    or (nr, nc) in visited
+                    or board[nr][nc] != "O"
+                ):
+                    continue
+                q.append((nr, nc))
 
-        bfs()
+    
 
         for r in range(ROWS):
             for c in range(COLS):
