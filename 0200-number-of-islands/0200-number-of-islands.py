d670caab3c3287d68bb8747cbe9cc82b09ad4f67
Dejoe John <22582871+dej0e@users.noreply.github.com>
2026-03-30 10:31:28 -0700

    Time: 257 ms (33.46%), Space: 26.8 MB (19.29%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -3,32 +3,34 @@ from collections import deque
 
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-        if not grid:
-            return 0
-        ROWS, COLS = len(grid), len(grid[0])
-        count = 0
-
+        ROWS = len(grid)
+        COLS = len(grid[0])
+        
         directions = [(-1, 0), (0, 1), (1, 0), (0, -1)]
+
+        visited = set()
         def bfs(row, col):
             q = deque()
             q.append((row, col))
+            visited.add((row, col))
             while q:
                 r, c = q.popleft()
                 for dr, dc in directions:
-                    nr = r + dr
-                    nc = c + dc 
-                    
-                    # check for valid cases
-                    if min(nr, nc) < 0 or nr >= ROWS or nc >= COLS or grid[nr][nc] == "0":
+                    nr, nc = dr + r, dc +c
+                    if min(nr, nc) < 0 or nr >= ROWS or nc >= COLS:
                         continue
-                    
+
+                    if (nr, nc) in visited or grid[nr][nc] != "1":
+                        continue
                     q.append((nr, nc))
+                    visited.add((nr, nc))
                     grid[nr][nc] = "0"
-            
-             
+        
+        res = 0
         for r in range(ROWS):
             for c in range(COLS):
                 if grid[r][c] == "1":
                     bfs(r, c)
-                    count += 1
-        return count
+                    res += 1
+        return res
+                    
