80e6aeffb0bbfab1d146540d4797a0bee7603a5b
Dejoe John <22582871+dej0e@users.noreply.github.com>
2026-03-30 23:44:00 -0700

    Time: 124 ms (55.79%), Space: 78.7 MB (25.9%) - LeetHub

--- 0146-lru-cache/0146-lru-cache.py
+++ 0146-lru-cache/0146-lru-cache.py
@@ -1,23 +1,48 @@
+class Node:
+    
+    def __init__(self, key, value):
+        self.key = key
+        self.value = value
+        self.prev = self.next = None
+
 class LRUCache:
     def __init__(self, capacity):
-        self.cache = OrderedDict()
+        self.cache = {} # map key to Node
         self.capacity = capacity
-    
+        self.left = Node(0,0) # LRU
+        self.right = Node(0,0) # most recent
+        self.left.next = self.right
+        self.right.prev = self.left
+
+    def remove(self, node): #remove node from list
+        prev, nxt = node.prev, node.next
+        prev.next = nxt
+        nxt.prev = prev
+
+    def insert(self, node): #insert at right 
+        right = self.right
+        prev = self.right.prev
+        node.next = right
+        node.prev = prev
+        prev.next = node
+        right.prev = node
+
     def get(self, key: int) -> int:
-        if key not in self.cache:
-            return -1
-        
-        value = self.cache[key]
-        self.cache.move_to_end(key)
-        return value
+        if key in self.cache:
+            self.remove(self.cache[key])
+            self.insert(self.cache[key])
+            return self.cache[key].value
+        return -1
     
     def put(self, key: int, value: int):
-        self.cache[key] = value
-        self.cache.move_to_end(key)
+        if key in self.cache:
+            self.remove(self.cache[key])
+        self.cache[key] = Node(key, value)
+        self.insert(self.cache[key])
         if len(self.cache) > self.capacity:
-            self.cache.popitem(last=False)
-        
-        
+            lru = self.left.next
+            self.remove(lru)    
+            del self.cache[lru.key] 
 
 # Your LRUCache object will be instantiated and called as such:
 # obj = LRUCache(capacity)
