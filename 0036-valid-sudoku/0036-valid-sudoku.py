4864d4ea0cb51038b523488ee8fe059585a8c023
Dejoe John <22582871+dej0e@users.noreply.github.com>
2026-03-31 20:15:46 -0700

    Time: 3 ms (76.29%), Space: 19.3 MB (85.68%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.py
+++ 0036-valid-sudoku/0036-valid-sudoku.py
@@ -3,16 +3,18 @@ class Solution:
         rows = defaultdict(set)
         cols = defaultdict(set)
         squares = defaultdict(set)
-        for r in range(9):
+
+        for r in range(9): 
             for c in range(9):
-                num = board[r][c]
-                if num == ".":
+                if board[r][c] == ".":
                     continue
-
-                if num in rows[r] or num in cols[c] or num in squares[(r//3, c//3)]:
+                
+                value = board[r][c]
+                square = (r//3, c//3)
+                if value in rows[r] or value in cols[c] or value in squares[square]:
                     return False
-
-                rows[r].add(num)
-                cols[c].add(num)
-                squares[(r//3, c//3)].add(num)
+                
+                rows[r].add(value)
+                cols[c].add(value)
+                squares[square].add(value)
         return True
