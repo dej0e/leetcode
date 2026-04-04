03c9115b862a749db98068a710798280e9a5de1e
Dejoe John <22582871+dej0e@users.noreply.github.com>
2026-04-04 10:32:17 -0700

    Time: 26 ms (29.07%), Space: 19.6 MB (27.66%) - LeetHub

--- 0040-combination-sum-ii/0040-combination-sum-ii.py
+++ 0040-combination-sum-ii/0040-combination-sum-ii.py
@@ -6,21 +6,16 @@ class Solution:
             if total == target:
                 res.append(curr.copy())
                 return
-            if total > target or i == len(candidates):
-                return
             
+            if i >= len(candidates) or total > target:
+                return 
+
             for j in range(i, len(candidates)):
-                # Do not allow the same number in the same recursion level. Skip it. 
-                # In deeper recursion levels, this would work as at that point j == i.
                 if j > i and candidates[j] == candidates[j - 1]:
                     continue
-               
-                if total + candidates[j] > target:
-                    break
-
                 curr.append(candidates[j])
                 dfs(j + 1, curr, total + candidates[j])
                 curr.pop()
-         
         dfs(0, [], 0)
         return res
+        
