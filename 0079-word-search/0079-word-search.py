6e248e6672240f914f58900771b6a5f923484a48
Dejoe John <22582871+dej0e@users.noreply.github.com>
2026-04-04 14:12:27 -0700

    Time: 4684 ms (22.37%), Space: 19.5 MB (40.88%) - LeetHub

--- 0079-word-search/0079-word-search.py
+++ 0079-word-search/0079-word-search.py
@@ -1,30 +1,25 @@
 class Solution:
     def exist(self, board: List[List[str]], word: str) -> bool:
-        
         ROWS = len(board)
         COLS = len(board[0])
         visited = set()
-
+        direction = [(-1, 0), (0, 1), (1, 0), (0, -1)]
         def dfs(r, c, i):
             if i == len(word):
                 return True
             
-            if r < 0 or c < 0 or r >= ROWS or c >= COLS or board[r][c] != word[i] or (r,c) in visited:
+            if min(r, c) < 0 or r >= ROWS or c >= COLS or (r, c) in visited or board[r][c] != word[i]:
                 return False
-            
-            visited.add((r, c))
-            directions = [(-1, 0), (0, 1), (1, 0), (0, -1)]
-            for dr, dc in directions:
-                nr = r + dr
-                nc = c + dc
-                if dfs(nr, nc, i + 1):
-                    visited.remove((r, c))
-                    return True
+
+            visited.add((r,c))
+            found = (dfs(r-1, c, i + 1) or 
+                    dfs(r, c + 1, i + 1) or 
+                    dfs(r + 1, c, i + 1) or 
+                    dfs(r, c - 1, i + 1))
+            visited.remove((r,c))
+            return found
 
 
-            visited.remove((r, c))
-            return False
-        
         for r in range(ROWS):
             for c in range(COLS):
                 if dfs(r, c, 0):
