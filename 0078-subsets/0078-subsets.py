b9f06e3660d6ec5e0df5f13a36d6c3c3acec136f
Dejoe John <22582871+dej0e@users.noreply.github.com>
2026-04-04 09:12:19 -0700

    Time: 0 ms (100%), Space: 19.4 MB (45.77%) - LeetHub

--- 0078-subsets/0078-subsets.py
+++ 0078-subsets/0078-subsets.py
@@ -1,21 +1,15 @@
 class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
         res = []
-        path = []
-        n = len(nums)
+        subset = []
         def dfs(i):
-            if i >= n:
-                if path not in res:
-                    res.append(path.copy())
+            if i >= len(nums):
+                res.append(subset[:])
                 return
             
-            # Adding current number to path
-            path.append(nums[i])
-            dfs(i+1)
-
-            # Without adding current number to path
-            path.pop()
-            dfs(i+1)
-                
+            subset.append(nums[i])
+            dfs(i + 1)
+            subset.pop()
+            dfs(i + 1)
         dfs(0)
         return res
