bac17339799b0f155884771f5b627f888e789337
Dejoe John <22582871+dej0e@users.noreply.github.com>
2026-04-04 09:13:48 -0700

    Time: 0 ms (100%), Space: 19.5 MB (16.71%) - LeetHub

--- 0078-subsets/0078-subsets.py
+++ 0078-subsets/0078-subsets.py
@@ -7,8 +7,11 @@ class Solution:
                 res.append(subset[:])
                 return
             
+            # decision to include nums[i]
             subset.append(nums[i])
             dfs(i + 1)
+
+            # decision to not include nums[i] (remove from subset, because we added it above.)
             subset.pop()
             dfs(i + 1)
         dfs(0)
