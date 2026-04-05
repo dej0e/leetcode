d13be84f4dda22e8b9d4957a51044341474e5dba
Dejoe John <22582871+dej0e@users.noreply.github.com>
2026-04-05 12:49:37 -0700

    Time: 3 ms (57.36%), Space: 19.5 MB (97.03%) - LeetHub

--- 0684-redundant-connection/0684-redundant-connection.py
+++ 0684-redundant-connection/0684-redundant-connection.py
@@ -1,36 +1,43 @@
+class DSU:
+    def __init__(self, n):
+        self.par = {}
+        self.rank = {}
+        for i in range(n + 1):
+            self.par[i] = i
+            self.rank[i] = 0
+        
+    
+    def find(self, n):
+        p = self.par[n]
+        while p != self.par[p]:
+            self.par[p] = self.par[self.par[p]]
+            p = self.par[p]
+        return p
+    
+    def union(self, n1, n2):
+        p1, p2 = self.find(n1), self.find(n2)
+        if p1 == p2: ## CYCLE!
+            return False
+        
+        if self.rank[p1] > self.rank[p2]:
+            self.par[p2] = p1
+        elif self.rank[p1] < self.rank[p2]:
+            self.par[p1] = p2
+        else:
+            self.par[p1] = p2
+            self.rank[p2] += 1
+        return True
+
+
+
 class Solution:
     def findRedundantConnection(self, edges: List[List[int]]) -> List[int]:
-        n_edges = len(edges)
-        n_vertices = n_edges + 1  
         adj = defaultdict(list)
-        for src, dst in edges:
-            adj[src].append(dst)
-            adj[dst].append(src)
-
-        result = []
-        visited = [False] * n_vertices
-        cycle = set()
-        cycleStart = -1
-        def dfs(node, parent):
-            nonlocal cycleStart
-            if visited[node]:
-                cycleStart = node
-                return True
-            
-            visited[node] = True
-            for nei in adj[node]:
-                if nei == parent:
-                    continue
-                if dfs(nei, node):
-                    if cycleStart != -1:
-                        cycle.add(node)
-                    if node == cycleStart:
-                        cycleStart = -1
-                    return True
-            return False
-        dfs(1,-1)
-        for u, v in reversed(edges):
-            if u in cycle and v in cycle:
-                return [u,v]
+        for u, v in edges:
+            adj[u].append(v)
+            adj[v].append(u)
+        dsu = DSU(len(adj)) 
+        for u, v in edges:
+            if not dsu.union(u, v):
+                return [u, v]
         return []
-                
