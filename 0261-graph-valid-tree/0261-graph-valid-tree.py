15f2cbeb96a7d462db0c1bf44cbd59527489b9d3
Dejoe John <22582871+dej0e@users.noreply.github.com>
2026-04-05 10:44:00 -0700

    Time: 0 ms (100%), Space: 20.3 MB (77.64%) - LeetHub

--- 0261-graph-valid-tree/0261-graph-valid-tree.py
+++ 0261-graph-valid-tree/0261-graph-valid-tree.py
@@ -3,25 +3,30 @@ from enum import Enum
 
 class Solution:
     def validTree(self, n: int, edges: List[List[int]]) -> bool:
-        if len(edges) > n-1:
+        if len(edges) > n - 1:
             return False
-        adj = {i:[] for i in range(n)}
-        for n1, n2 in edges:
-            adj[n1].append(n2)
-            adj[n2].append(n1)
         
-        #bfs
-        q = deque()
+        adj = {i: [] for i in range(n)}
+        for v1, v2 in edges:
+            adj[v1].append(v2)
+            adj[v2].append(v1)
+        
+        q = deque() #node, parent
         q.append((0, -1))
         visited = set()
         while q:
             for _ in range(len(q)):
                 node, parent = q.popleft()
+                
+                if node in visited:
+                    return False
+
                 visited.add(node)
-                for neighbor in adj[node]:
-                    if neighbor == parent:
+                for nei in adj[node]:
+                    if nei == parent:
                         continue
-                    if neighbor in visited:
-                        return False # no cycles
-                    q.append((neighbor, node))
-        return len(visited) == n
+                    q.append((nei, node))
+
+
+
+        return True if len(visited) == n else False
