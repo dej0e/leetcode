8b50db3a0917260e343ad51b5da8b57b2da9c728
Dejoe John <22582871+dej0e@users.noreply.github.com>
2026-04-05 00:41:53 -0700

    Time: 38 ms (53.85%), Space: 22 MB (78.4%) - LeetHub

--- 0286-walls-and-gates/0286-walls-and-gates.py
+++ 0286-walls-and-gates/0286-walls-and-gates.py
@@ -8,23 +8,25 @@ class Solution:
         """
         Do not return anything, modify rooms in-place instead.
         """
-        ROWS, COLS = len(rooms), len(rooms[0])
+        ROWS = len(rooms)
+        COLS = len(rooms[0])
         q = deque()
+        directions = [(-1, 0), (0, 1), (1, 0), (0, -1)]
         for r in range(ROWS):
             for c in range(COLS):
                 if rooms[r][c] == 0:
                     q.append((r, c))
+        
         level = 1
-        directions = [(-1, 0), (0, 1), (1, 0), (0, -1)]
-
         while q:
-            for i in range(len(q)):
+            for _ in range(len(q)):
                 r, c = q.popleft()
                 for dr, dc in directions:
-                    nr, nc = dr+r, dc+c
+                    nr, nc = dr + r, dc + c
                     if min(nr, nc) < 0 or nr >= ROWS or nc >= COLS or rooms[nr][nc] != INF:
                         continue
                     q.append((nr, nc))
                     rooms[nr][nc] = level
+
             level += 1
-        
+        return rooms
