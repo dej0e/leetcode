1ad043810c6f25b9da2c212840a8b0883e82472f
Dejoe John <22582871+dej0e@users.noreply.github.com>
2026-04-05 10:49:17 -0700

    Time: 0 ms (100%), Space: 20.3 MB (93.18%) - LeetHub

--- 0261-graph-valid-tree/0261-graph-valid-tree.py
+++ 0261-graph-valid-tree/0261-graph-valid-tree.py
@@ -5,28 +5,25 @@ class Solution:
     def validTree(self, n: int, edges: List[List[int]]) -> bool:
         if len(edges) > n - 1:
             return False
-        
+
         adj = {i: [] for i in range(n)}
         for v1, v2 in edges:
             adj[v1].append(v2)
             adj[v2].append(v1)
-        
-        q = deque() #node, parent
-        q.append((0, -1))
+
+        q = deque()  # node, parent
         visited = set()
+        q.append((0, -1))
+        visited.add(0)
         while q:
-            for _ in range(len(q)):
-                node, parent = q.popleft()
-                
-                if node in visited:
+            node, parent = q.popleft()
+
+            for nei in adj[node]:
+                if nei == parent:
+                    continue
+                if nei in visited:
                     return False
+                visited.add(nei)
+                q.append((nei, node))
 
-                visited.add(node)
-                for nei in adj[node]:
-                    if nei == parent:
-                        continue
-                    q.append((nei, node))
-
-
-
-        return True if len(visited) == n else False
+        return len(visited) == n
