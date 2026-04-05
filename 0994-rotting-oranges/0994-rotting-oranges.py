c3c30821a80b6c5255b168cb232da2d3b6bd1ab1
Dejoe John <22582871+dej0e@users.noreply.github.com>
2026-04-05 00:58:40 -0700

    Time: 2 ms (81.05%), Space: 19.3 MB (85.79%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.py
+++ 0994-rotting-oranges/0994-rotting-oranges.py
@@ -8,17 +8,18 @@ class Solution:
         directions = [(-1, 0), (0, 1), (1, 0), (0, -1)]
         q = deque()
         fresh = 0
+        minutes = 0
         for r in range(ROWS):
             for c in range(COLS):
                 if grid[r][c] == 1:
                     fresh += 1
                 if grid[r][c] == 2:
                     q.append((r, c))
-        
-        minutes = 0
+
         while fresh > 0 and q: 
             for _ in range(len(q)):
                 r, c = q.popleft()
+                
                 for dr, dc in directions:
                     nr, nc = dr + r, dc + c
                     if min(nr, nc) < 0 or nr >= ROWS or nc >= COLS or grid[nr][nc] != 1:
@@ -26,6 +27,6 @@ class Solution:
                     grid[nr][nc] = 2
                     q.append((nr, nc))
                     fresh -= 1
-                    
             minutes += 1
-        return -1 if fresh > 0 else minutes
+        
+        return minutes if fresh == 0 else -1
