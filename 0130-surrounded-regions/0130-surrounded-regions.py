13a192257bf255c5361beaa185a3050c15a9959b
Dejoe John <22582871+dej0e@users.noreply.github.com>
2026-04-05 01:21:45 -0700

    Time: 0 ms (100%), Space: 22.2 MB (94.21%) - LeetHub

--- 0130-surrounded-regions/0130-surrounded-regions.py
+++ 0130-surrounded-regions/0130-surrounded-regions.py
@@ -8,36 +8,35 @@ class Solution:
         """
         ROWS = len(board)
         COLS = len(board[0])
-        directions = [(-1, 0), (0, 1), (1, 0), (0, -1)]
-        visited = set()
-
         q = deque()
+        
         for r in range(ROWS):
-            for c in range(COLS):
-                if (r == 0 or r == ROWS - 1 or c == 0 or c == COLS - 1) and board[r][c] == "O":
-                    q.append((r, c))
-        while q:
-            r, c = q.popleft()
-            visited.add((r, c))
+            if board[r][0] == "O":
+                q.append((r, 0))
+            if board[r][COLS - 1] == "O":
+                q.append((r, COLS-1))
+
+        for c in range(COLS):
+            if board[0][c] == "O":
+                q.append((0, c))
+            if board[ROWS - 1][c] == "O":
+                q.append((ROWS - 1, c))
+
+        directions = [(-1, 0), (0, 1), (1, 0), (0, -1)]
+        while q:   
+            r, c  = q.popleft()
             board[r][c] = "T"
             for dr, dc in directions:
-                nr = dr + r
-                nc = dc + c
-                if (
-                    min(nr, nc) < 0
-                    or nr >= ROWS
-                    or nc >= COLS
-                    or (nr, nc) in visited
-                    or board[nr][nc] != "O"
-                ):
+                nr, nc = r + dr, c + dc
+                if min(nr, nc) < 0 or nr >= ROWS or nc >= COLS or board[nr][nc] != "O":
                     continue
+                board[nr][nc] = "T"
                 q.append((nr, nc))
-
-    
-
+        
         for r in range(ROWS):
             for c in range(COLS):
                 if board[r][c] == "O":
                     board[r][c] = "X"
-                elif board[r][c] == "T":
+                if board[r][c] == "T":
                     board[r][c] = "O"
+        return board
