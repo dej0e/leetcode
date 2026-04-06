0365b2898c54edb828eb8cfe7b3bb2af961244ec
Dejoe John <22582871+dej0e@users.noreply.github.com>
2026-04-05 18:47:33 -0700

    Time: 1277 ms (27.41%), Space: 27.1 MB (8.42%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
@@ -1,14 +1,17 @@
 class Solution:
     def longestPalindrome(self, s: str) -> str:
         n = len(s)
-        dp = [[False] * n for _ in range(n)]
-        resIndex = 0
+        resIdx = -1
         resLen = 0
-        for i in range(n - 1, -1, -1):
+        dp = [[False] * n for _ in range(n)]
+        for i in range(n-1, -1, -1):
             for j in range(i, n):
                 if s[i] == s[j] and (j - i <= 2 or dp[i + 1][j - 1] == True):
                     dp[i][j] = True
                     if (j - i + 1) > resLen:
-                        resIndex = i
-                        resLen = j - i + 1
-        return s[resIndex : resIndex + resLen]
+                        resLen = j - i  + 1
+                        resIdx = i
+
+
+
+        return s[resIdx : resIdx + resLen]
