21da903edf7864b2348d0cd0dc4fe932b74cff5f
Dejoe John <22582871+dej0e@users.noreply.github.com>
2026-04-06 13:15:31 -0700

    Time: 7 ms (46.06%), Space: 21.5 MB (12.73%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.py
+++ 0207-course-schedule/0207-course-schedule.py
@@ -4,31 +4,56 @@ from collections import deque
 
 class Solution:
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
-        premap = defaultdict(list)
-        for crs, pre in prerequisites:
-            premap[crs].append(pre)
+        # premap = defaultdict(list)
+        # for crs, pre in prerequisites:
+        #     premap[crs].append(pre)
         
-        visiting = set()
-        def dfs(crs):
-            if crs in visiting:
-                return False
+        # visiting = set()
+        # def dfs(crs):
+        #     if crs in visiting:
+        #         return False
             
-            if premap[crs] == []:
-                return True
+        #     if premap[crs] == []:
+        #         return True
             
-            visiting.add(crs)
-            for pre in premap[crs]:
-                if not dfs(pre):
-                    return False
+        #     visiting.add(crs)
+        #     for pre in premap[crs]:
+        #         if not dfs(pre):
+        #             return False
             
-            visiting.remove(crs)
-            premap[crs] = []
-            return True
+        #     visiting.remove(crs)
+        #     premap[crs] = []
+        #     return True
 
             
         
-        for crs in range(numCourses):
-            if not dfs(crs):
-                return False
+        # for crs in range(numCourses):
+        #     if not dfs(crs):
+        #         return False
         
+        # return True
+
+        prereq = {c: [] for c in range(numCourses)}
+        for crs, pre in prerequisites:
+            prereq[crs].append(pre)
+
+        visit, cycle = set(), set()
+
+        def dfs(crs):
+            if crs in cycle:
+                return False
+            if crs in visit:
+                return True
+
+            cycle.add(crs)
+            for pre in prereq[crs]:
+                if dfs(pre) == False:
+                    return False
+            cycle.remove(crs)
+            visit.add(crs)
+            return True
+
+        for c in range(numCourses):
+            if dfs(c) == False:
+                return False
         return True
