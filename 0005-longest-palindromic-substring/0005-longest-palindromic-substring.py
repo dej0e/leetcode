ef30434aa9541935874b01531e7e38df0e72d048
Dejoe John <22582871+dej0e@users.noreply.github.com>
2026-04-05 18:50:14 -0700

    Time: 239 ms (79.05%), Space: 19.3 MB (80.23%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
@@ -1,17 +1,36 @@
 class Solution:
     def longestPalindrome(self, s: str) -> str:
+        # n = len(s)
+        # resIdx = -1
+        # resLen = 0
+        # dp = [[False] * n for _ in range(n)]
+        # for i in range(n-1, -1, -1):
+        #     for j in range(i, n):
+        #         if s[i] == s[j] and (j - i <= 2 or dp[i + 1][j - 1] == True):
+        #             dp[i][j] = True
+        #             if (j - i + 1) > resLen:
+        #                 resLen = j - i  + 1
+        #                 resIdx = i
+
+        # return s[resIdx : resIdx + resLen]
+
         n = len(s)
-        resIdx = -1
+        res = ""
         resLen = 0
-        dp = [[False] * n for _ in range(n)]
-        for i in range(n-1, -1, -1):
-            for j in range(i, n):
-                if s[i] == s[j] and (j - i <= 2 or dp[i + 1][j - 1] == True):
-                    dp[i][j] = True
-                    if (j - i + 1) > resLen:
-                        resLen = j - i  + 1
-                        resIdx = i
+        for i in range(n):
+            l, r = i, i
+            while l >= 0 and r < n and s[l] == s[r]:
+                if (r - l + 1) > resLen:
+                    res = s[l : r + 1]
+                    resLen = r - l + 1
+                l -= 1
+                r += 1
 
-
-
-        return s[resIdx : resIdx + resLen]
+            l, r = i, i + 1
+            while l >= 0 and r < n and s[l] == s[r]:
+                if (r - l + 1) > resLen:
+                    res = s[l : r + 1]
+                    resLen = r - l + 1
+                l -= 1
+                r += 1
+        return res
