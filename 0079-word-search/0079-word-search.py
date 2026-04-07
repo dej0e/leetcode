238b53cbbc858ae0e967b575edbe6a3e265160ea
Dejoe John <22582871+dej0e@users.noreply.github.com>
2026-04-07 12:10:31 -0700

    Time: 4703 ms (21.91%), Space: 19.6 MB (39.8%) - LeetHub

--- 0079-word-search/0079-word-search.py
+++ 0079-word-search/0079-word-search.py
@@ -2,26 +2,22 @@ class Solution:
     def exist(self, board: List[List[str]], word: str) -> bool:
         ROWS = len(board)
         COLS = len(board[0])
-        
+        visited = set()
         def dfs(r, c, i):
-            if i == len(word):
+            if i >= len(word):
                 return True
-            
-            if min(r, c) < 0 or r >= ROWS or c >= COLS or board[r][c] == "#" or board[r][c] != word[i]:
+
+            if min(r,c) < 0 or r >= ROWS or c >= COLS or (r, c) in visited or board[r][c] != word[i] :
                 return False
+            
+            visited.add((r, c))
+            found = dfs(r - 1, c, i + 1) or dfs(r, c + 1, i + 1) or dfs(r + 1, c, i + 1) or dfs(r, c - 1, i + 1)
+            visited.remove((r, c))
 
-            board[r][c] = "#"
-            found = (dfs(r-1, c, i + 1) or 
-                    dfs(r, c + 1, i + 1) or 
-                    dfs(r + 1, c, i + 1) or 
-                    dfs(r, c - 1, i + 1))
-            board[r][c] = word[i]
             return found
-
 
         for r in range(ROWS):
             for c in range(COLS):
-
-                if board[r][c] == word[0] and dfs(r, c, 0):
+                if dfs(r, c, 0):
                     return True
         return False
