74617d852951c61a07a990ec6b858be84376173a
Dejoe John <22582871+dej0e@users.noreply.github.com>
2026-04-07 18:45:58 -0700

    Time: 55 ms (68.5%), Space: 19.5 MB (59.07%) - LeetHub

--- 0680-valid-palindrome-ii/0680-valid-palindrome-ii.py
+++ 0680-valid-palindrome-ii/0680-valid-palindrome-ii.py
@@ -1,21 +1,23 @@
 class Solution:
     def validPalindrome(self, s: str) -> bool:
 
-        def isPali(st):
-            l, r = 0, len(st) - 1
-            while l < r:
-                if st[l] != st[r]:
-                    return False
-                l += 1
-                r -= 1
-            return True
+    
 
         l, r = 0, len(s) - 1
         while l < r:
             if s[l] != s[r]:
                 skipLeft = s[l + 1 : r + 1]
                 skipRight = s[l:r]
-                return isPali(skipLeft) or isPali(skipRight)
+                return self.isPali(skipLeft) or self.isPali(skipRight)
+            l += 1
+            r -= 1
+        return True
+
+    def isPali(self, st):
+        l, r = 0, len(st) - 1
+        while l < r:
+            if st[l] != st[r]:
+                return False
             l += 1
             r -= 1
         return True
