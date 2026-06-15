ce0a06b8bedef712f4255aaedcafa20f68d66340
Dejoe John <22582871+dej0e@users.noreply.github.com>
2026-06-15 12:16:48 -0700

    Time: 0 ms (100%), Space: 19.2 MB (61.65%) - LeetHub

--- 0198-house-robber/0198-house-robber.py
+++ 0198-house-robber/0198-house-robber.py
@@ -1,22 +1,35 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
         n = len(nums)
-        dp = [-1] * n
-        def houserob(ind):
-            if ind == 0:
-                return nums[0]
-            if ind < 0:
-                return 0
-            if dp[ind] != -1:
-                return dp[ind]
+        #Memoization
+        # dp = [-1] * n
+        # def houserob(ind):
+        #     if ind == 0:
+        #         return nums[0]
+        #     if ind < 0:
+        #         return 0
+        #     if dp[ind] != -1:
+        #         return dp[ind]
 
-            pick = nums[ind]
-            if ind > 1: 
-                pick += houserob(ind - 2)
+        #     pick = nums[ind]
+        #     if ind > 1: 
+        #         pick += houserob(ind - 2)
             
-            notPick = 0 + houserob(ind - 1)
+        #     notPick = 0 + houserob(ind - 1)
 
-            dp[ind] = max(pick, notPick)
-            return dp[ind]
+        #     dp[ind] = max(pick, notPick)
+        #     return dp[ind]
 
-        return houserob(n-1)
+        # return houserob(n-1)
+
+        #Tabulation
+        dp = [-1] * n
+        dp[0] = nums[0]
+        for i in range(1, n):
+            pick = nums[i]
+            if i > 1:
+                pick += dp[i - 2]
+            
+            notPick = 0 + dp[i - 1]
+            dp[i] = max(pick, notPick)
+        return dp[n-1]
